a1b5f645d116cb63385f31960e27044edc93472d
Jeremiah Grande <jeregrande@yahoo.com>
2019-10-03 07:21:17 -0700

    Authentication navigation

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ const uiConfig = {
         },
     },
     signInFlow: 'popup',
-    signInSuccessUrl: 'signedIn',
+    signInSuccessUrl: 'home.html', //Default: signedIn
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
